19b141ffa06110c34f7050d2b80cea9bbe1908f8
BGowera <91614476+BGowera@users.noreply.github.com>
2022-07-11 15:33:46 +0200

    created functions for user input and to play round

--- js/script.js
+++ js/script.js
@@ -20,3 +20,42 @@ function computerPlay(){
     return value;
 }
 
+// function for player input
+function userPlay() {
+    let userPrompt = prompt('Rock,Paper or Scissors?');
+    return userPrompt.toLowerCase();
+}
+
+// function to play a single round
+
+let playerScore = 0;
+let computerScore = 0;
+let winner
+function playRound() {
+
+   let userChoice = userPlay();
+    let computerChoice = computerPlay();
+// win scenarios
+    if (userChoice == 'rock' && computerChoice == 'scissors'||userChoice == 'paper' && computerChoice == 'rock'||userChoice=='scissors'&& computerChoice=='paper'){
+        playerScore++;
+        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+        winner = 'You win!!!';
+    }
+
+    // lose scenario
+       else if (userChoice == 'rock' && computerChoice == 'paper'||userChoice == 'paper' && computerChoice == 'scissors'||userChoice=='scissors'&&computerChoice=='rock'){
+        computerScore++;
+        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+        winner = 'Computer wins!!!';
+    }
+// draw scenario
+else if (userChoice == 'rock' && computerChoice == 'rock'||userChoice == 'paper' && computerChoice == 'paper'||userChoice=='scissors'&&computerChoice=='scissors'){
+        playerScore++;
+        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+        winner = 'Draw Game!!!';
+    }
+    
+return winner
+}
+
+console.log(playRound());
