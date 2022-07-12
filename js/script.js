9b824208dd283cb2eb20baa14f0f0cd19fc6edd1
BGowera <91614476+BGowera@users.noreply.github.com>
2022-07-12 05:43:35 +0200

    added confirm

--- js/script.js
+++ js/script.js
@@ -1,86 +1,97 @@
-
+const wannaPlay = confirm('Do you want to play with me?');
 
 // function for computer turns
-
-function computerPlay(){
-    const options = ['rock', 'paper', 'scissors'];
-    let randomNum = Math.floor(Math.random() * 3);
-    let value;
-    switch (randomNum) {
-        case 0:
-            value = options[0];
-            break;
-        case 1:
-            value = options[1];
-            break;
-        case 2:
-            value = options[2];
-            break;
-        default:
-            value = 'Something went wrong';
-    }
-    return value;
-}
-
-// function for player input
-function userPlay() {
-    let userPrompt = prompt('Rock,Paper or Scissors?');
-    return userPrompt.toLowerCase();
-}
-
-// function to play a single round
-
-let playerScore = 0;
-let computerScore = 0;
-
-function playRound() {
-let winner;
-   let userChoice = userPlay();
-    let computerChoice = computerPlay();
-// win scenarios
-    if (userChoice == 'rock' && computerChoice == 'scissors'||userChoice == 'paper' && computerChoice == 'rock'||userChoice=='scissors'&& computerChoice=='paper'){
-        playerScore++;
-        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
-        winner = 'You win!!!';
+if (wannaPlay) {
+    function computerPlay() {
+        const options = ['rock', 'paper', 'scissors'];
+        let randomNum = Math.floor(Math.random() * 3);
+        let value;
+        switch (randomNum) {
+            case 0:
+                value = options[0];
+                break;
+            case 1:
+                value = options[1];
+                break;
+            case 2:
+                value = options[2];
+                break;
+            default:
+                value = 'Something went wrong';
+        }
+        return value;
     }
 
-    // lose scenario
-       else if (userChoice == 'rock' && computerChoice == 'paper'||userChoice == 'paper' && computerChoice == 'scissors'||userChoice=='scissors'&&computerChoice=='rock'){
-        computerScore++;
-        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
-        winner = 'Computer wins!!!';
+    // function for player input
+    function userPlay() {
+        let userPrompt = prompt('Rock,Paper or Scissors?');
+        return userPrompt.toLowerCase();
     }
-// draw scenario
-else if (userChoice == 'rock' && computerChoice == 'rock'||userChoice == 'paper' && computerChoice == 'paper'||userChoice=='scissors'&&computerChoice=='scissors'){
+
+    // function to play a single round
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function playRound() {
+        let winner;
+        let userChoice = userPlay();
+        let computerChoice = computerPlay();
+        // win scenarios
+        if (userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'paper' && computerChoice == 'rock' || userChoice == 'scissors' && computerChoice == 'paper') {
+            playerScore++;
+            console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+            winner = 'You win!!!';
+        }
+
+        // lose scenario
+        else if (userChoice == 'rock' && computerChoice == 'paper' || userChoice == 'paper' && computerChoice == 'scissors' || userChoice == 'scissors' && computerChoice == 'rock') {
+            computerScore++;
+            console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+            winner = 'Computer wins!!!';
+        }
+        // draw scenario
+        else if (userChoice == 'rock' && computerChoice == 'rock' || userChoice == 'paper' && computerChoice == 'paper' || userChoice == 'scissors' && computerChoice == 'scissors') {
    
-        console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
-        winner = 'Draw Game!!!';
+            console.log(`You chose ${userChoice} and computer chose ${computerChoice}`);
+            winner = 'Draw Game!!!';
+        }
+        else {
+            winner = 'Please check your input';
     }
-    
-return winner
+        return winner
+    }
+
+    // function for 5 round game
+
+ 
+
+    function playGame() {
+        let overallWinner;
+        console.log('Best of 5 let\'s go!');
+        for (let i = 1; i < 6; i++) {
+            console.log(`Round${i}`);
+            console.log(playRound());
+            console.log(`Your score-${playerScore}`)
+            console.log(`Computer score-${computerScore}`)
+        }
+        if (playerScore > computerScore) {
+            overallWinner = 'Player 1 Wins The Game';
+        }
+        else if (computerScore > playerScore) {
+            overallWinner = 'Computer Wins The Game';
+        }
+        else if (computerScore == playerScore) {
+            overallWinner = 'Draw Game';
+        }
+        return overallWinner;
+    }
+
+    playGame()
+
+ 
 }
 
-// function for 5 round game
-
-
-
-function playGame() {
-    let overallWinner;
-    console.log('Best of 5 let\'s go!');
-    for (let i = 1; i < 6; i++){
-        console.log(`Round${i}`);
-        console.log(playRound());
-        console.log(`Your score-${playerScore}`)
-        console.log(`Computer score-${computerScore}`)
-    }
-    if (playerScore > computerScore) {
-    overallWinner='Player 1 Wins';
-    }
-    else if (computerScore > playerScore) {
-        overallWinner = 'Computer wins';
-    }
-    else if (computerScore == playerScore) {
-        overallWinner = 'Draw Game';
-    }
-    return overallWinner;
+else {
+    alert('Guess not-maybe later?')
 }
